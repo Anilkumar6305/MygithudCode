c68d6743e1add8becdcbaf4ff93f5b4e3aa08d08
root <root@ip-172-31-69-213.ec2.internal>
2025-11-22 07:49:27 +0000

    Added the java code in new branch

---
+++ ap.java
@@ -0,0 +1,8 @@
+//Adding the vesrion2 code in github
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i); // Prints numbers from 1 to 5
+        }
+    }
+}
